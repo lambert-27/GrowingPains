b20ca36f6100de5044a8d277fe5f3ad78d5aab10
Mark L <marklambert123@gmail.com>
2025-01-21 21:46:09 +0000

    Played around w/ connecting to plesk

--- GrowingPains/src/crud/driver.java
+++ GrowingPains/src/crud/driver.java
@@ -11,23 +11,27 @@ import java.sql.Statement;
 public class driver {
 		
 		public static void main(String[] args) {
-			//database URL
-			final String DATABSE_URL = "jdbc:mysql://localhost/plantsystem";
-				
+			//database URL using plesk + mariadb subprotocol
+			final String DATABSE_URL = "jdbc:mariadb://c00192497.candept.com:3306/GrowingPains";
+			final String USER_NAME = "user";
+			final String PASS_WORD = "Growing_up27";
+			
 			Connection connection = null;
 			Statement statement = null;
 			crud c;
 			c = new crud(connection, statement);
-			
-			String f_name = "Mark";
-			String l_name = "Lambert";
+
+			String f_name = "John";
+			String l_name = "nhoJ";
 			String password = "yo123";
 			String email = "NO";
 			String address = "YES";
 			int phone = 12393;
 		
 			try {
-				connection = DriverManager.getConnection(DATABSE_URL);
+				connection = DriverManager.getConnection(DATABSE_URL, USER_NAME, PASS_WORD);
+				//Create Statement for inserting into table
+				statement = connection.createStatement();
 				c.insertCustomer(f_name, l_name, email, address, password, phone);
 			} catch (SQLException e) {
 				// TODO Auto-generated catch block
@@ -35,7 +39,9 @@ public class driver {
 			}
 			finally{
 				try {
+					if(statement != null)
 					statement.close();
+					if(connection != null)
 					connection.close();
 				}
 				catch (Exception exception) {
